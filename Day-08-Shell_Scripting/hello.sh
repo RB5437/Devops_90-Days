b6de82d8acf469e35d922af9cb8d499a96f614b0
Ritik Bawane <Ritikbawane5437@gmail.com>
2026-04-20 15:11:07 +0530

    Update hello.sh

--- Day-08-Shell_Scripting/hello.sh
+++ Day-08-Shell_Scripting/hello.sh
@@ -2,8 +2,8 @@
 # 
 # This is script for RBB
 
-echo "RBB: Hello Dosto"
+echo "RITIK: Hello Dosto"
 
 echo "Learns: Devops wala bhaiya , hum toh Devops Padhenge"
 
-echo "RBB: Toh Dosto, like bhi kar do"
+echo "RITIK: Toh Dosto, like bhi kar do"
