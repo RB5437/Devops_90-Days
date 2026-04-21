65bb44f90b8e89bc71599f96b9d84faf0f9a5b8a
root <root@ip-172-31-46-169.ap-south-1.compute.internal>
2026-04-21 11:17:37 +0000

    added shell scripting practice files

---
+++ Day-09-Shell_Scripting/if-condition.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+<<disclaimer 
+This is just for infotainment purpose
+disclaimer
+
+read -p "tapu ki age: " age
+
+if [ $age -gt 18 ]
+then
+	echo "you are adult"
+fi
---
+++ Day-09-Shell_Scripting/until_Loop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+count=1
+
+until [ $count -gt 5 ]
+do
+  echo $count
+  ((count++))
+done
