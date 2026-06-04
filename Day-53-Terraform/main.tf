fd7c9266152e0b7a61bb7ffe15643f87b288b054
Ubuntu <ubuntu@ip-172-31-39-99.ec2.internal>
2026-06-04 10:25:25 +0000

    Add Day 53 Terraform

---
+++ Day-53-Terraform/main.tf
@@ -0,0 +1,4 @@
+resource local_file my_file {
+    filename = "automate.txt"
+    content = "Today i my DevOps_90_Days Day 53"
+}
---
+++ Day-53-Terraform/s3.tf
@@ -0,0 +1,6 @@
+#this is s3 bucket
+
+resource "aws_s3_bucket" "my_devops_bucket" {
+
+  bucket = "devops-terraform-bucket-172313999-2026"
+}
