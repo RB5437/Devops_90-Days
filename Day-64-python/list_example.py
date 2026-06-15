975d9fb05319fa0411c9d3b3d2d8f831ae710545
Ritik Bawane <Ritikbawane5437@gmail.com>
2026-06-15 15:47:46 +0530

    Add files via upload

---
+++ Day-64-python/list_example.py
@@ -0,0 +1,8 @@
+# list
+ 
+
+s3_bucket_list = ["ritik_demo_bucket", "mis_demo_bucket", "ayu_demo_bucket", "rik_demo_bucket"]
+#s3_bucket_list.append("john_demo_bucket")
+s3_bucket_list.remove("rik_demo_bucket")
+#print(s3_bucket_list)
+print(s3_bucket_list[2])
---
+++ Day-64-python/tuple.example.py
@@ -0,0 +1,4 @@
+#tuple
+s3_bucket_list = ("ritik_demo_bucket", "mis_demo_bucket", "ayu_demo_bucket", "rik_demo_bucket")
+
+print(type(s3_bucket_list))
